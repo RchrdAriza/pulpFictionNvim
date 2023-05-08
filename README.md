b289e12e739f01683192b832bbc6cfe2e8366efe
RchrdAlv <rchrdalv@gmail.com>
2023-05-08 11:26:33 -0500

    modifique el archivo README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 <p align="center">
-  <img src=".imgs/temaimg.jpg" width="300" height="200">
+  <img src=".imgs/temaimg.jpg" width="300" >
 </p>
---
+++ install.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/RchrdAlv/pulpFictionNvim-Theme
+
+cd pulpFictionNvim-Theme
+
+mkdir -p ~/.config/nvim/colors
+
+cp colors/* ~/.config/nvim/colors
