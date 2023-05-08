21ec6b7da217586f6e231b884d37f37c9136b82a
Richard Ariza <128317303+RchrdAlv@users.noreply.github.com>
2023-05-08 12:24:20 -0500

    Actualizar install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+RED='\033[0;31m' 
+GREEN='\033[0;32m' 
+YELLOW='\033[0;33m' 
+NC='\033[0m'
 
+echo -e "{AMARILLO}Cloning repository{NC}"
 git clone https://github.com/RchrdAlv/pulpFictionNvim-Theme
+sleep 2
+clear
 
+echo -e "{RED}Finishing some details{NC}"
 cd pulpFictionNvim-Theme
 
 mkdir -p ~/.config/nvim/colors
 
 cp colors/* ~/.config/nvim/colors
+sleep 2
+clear
+
+echo -e "{GREEN}Ready, you can now enjoy{NC}"
